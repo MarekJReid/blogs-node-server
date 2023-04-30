ba738d838d6fef788d7cd4067467f485da2cc1cc
Marek <marekreid@gmail.com>
2023-04-30 11:03:16 +0200

    added nonsense test for deployment

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-const http = require("http");
+describe("Math.random()", () => {
+  it("should return a number between 0 and 1", () => {
+    const randomNumber = Math.random();
+    expect(randomNumber).toBeGreaterThanOrEqual(0);
+    expect(randomNumber).toBeLessThanOrEqual(1);
+  });
 
-describe("Server", () => {
-  it("should return 200 OK", (done) => {
-    http.get("http://localhost:3000", (res) => {
-      expect(res.statusCode).toBe(200);
-      done();
-    });
+  it("should not always return the same number", () => {
+    const randomNumber1 = Math.random();
+    const randomNumber2 = Math.random();
+    expect(randomNumber1).not.toBe(randomNumber2);
   });
 });
